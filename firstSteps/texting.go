c99a59f1f16cbf1a23fcfa4ed13ec1aabd2625a3
swan <sc0p.sdc@gmail.com>
2019-02-25 21:24:25 +0100

    beginning with texts

---
+++ firstSteps/texting.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Demo",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+
+		win.Clear(colornames.Honeydew)
+
+		win.Update()
+
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+}
