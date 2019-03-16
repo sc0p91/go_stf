e7e9c4421c63ece7cb4ed102dd1fc20bb4e16f37
swan <sc0p.sdc@gmail.com>
2019-03-16 22:35:38 +0100

    first steps with http reqs

---
+++ httpTests/workbench.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// Simply defines the URL
+	url := "https://reqres.in/api/users"
+
+	// Creates the Request
+	req, _ := http.NewRequest("GET", url, nil)
+
+	// Adding optional Header to Request
+	req.Header.Add("cache-control", "no-cache")
+
+	// Actually does the Request
+	res, _ := http.DefaultClient.Do(req)
+
+	// Closes the Request after finishing
+	defer res.Body.Close()
+
+	// Collects all data from response
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// Prints the response data
+	fmt.Println(string(body))
+}
