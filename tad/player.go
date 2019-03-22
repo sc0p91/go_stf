1d1786f33e2b3352597b9e3c237bdd925a53a41b
Gabor Tanz <izolight@gmail.com>
2019-03-22 12:51:51 +0100

    put attacks and items into arrays

--- tad/player.go
+++ tad/player.go
@@ -1,25 +1,21 @@
 package main
 
 type player struct {
-	name string
-	class string
-	hp int
-	mp int
-	maxHp int
-	maxMp int
-	exp int
-	maxExp int
-	lvl int
-	sta int
-	str int
-	int int
-	dex int
-	atk1 string
-	atk2 string
-	atk3 string
-	atk4 string
-	itm1 string
-	itm2 string
-	hhi string
-	mhi string
+	name    string
+	class   string
+	hp      int
+	mp      int
+	maxHp   int
+	maxMp   int
+	exp     int
+	maxExp  int
+	lvl     int
+	sta     int
+	str     int
+	int     int
+	dex     int
+	attacks [4]string
+	items   [2]string
+	hhi     string
+	mhi     string
 }
