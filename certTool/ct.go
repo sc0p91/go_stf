bacccd6d5177b7adfb5415a5138001fbf5d5c2a0
swan <sc0p.sdc@gmail.com>
2019-10-11 14:51:31 +0200

    first alpha - args implemented

--- certTool/ct.go
+++ certTool/ct.go
@@ -16,9 +16,9 @@ func main() {
 	// Possible Arguments (Arg-Name, Arg-Default, Help-Text)
 	certiPrt := flag.String("in", "", "<certfile> in")
 	certoPrt := flag.String("out", "", "[certfile] out")
-	keyiPrt := flag.String("ki", "", "<certfile> in")
-	keyoPrt := flag.String("ko", "", "<certfile> out")
-	cvrtfPrt := flag.String("f", "", "format: {x509|pkcs7|pkcs12|der}")
+	keyiPrt := flag.String("ki", "", "<keyfile> in")
+	keyoPrt := flag.String("ko", "", "[keyfile] out")
+	cvrtfPrt := flag.String("f", "", "format: {x509|pkcs7|pkcs12}")
 
 	// Parsing the Flags
 	flag.Parse()
@@ -37,14 +37,11 @@ func main() {
 			extension = "crt"
 			ConvPem(*certiPrt)
 		case "pkcs7":
-			extension = "pk"
+			extension = "cer"
 			ConvP7(*certiPrt)
 		case "pkcs12":
 			extension = "p12"
 			ConvP12(*certiPrt)
-		case "der":
-			extension = "der"
-			ConvDer(*certiPrt)
 		default:
 			fmt.Printf("Unsupported format: %s", *cvrtfPrt)
 			flag.PrintDefaults()
@@ -77,9 +74,4 @@ func ConvP7(certiPrt string) {
 //ConvP12 - Does the conversion with pkcs12 certs
 func ConvP12(certiPrt string) {
 	fmt.Printf("inside pkcs12 func\n")
-}
-
-//ConvDer - Does the conversion with der certs
-func ConvDer(certiPrt string) {
-	fmt.Printf("inside der func\n")
 }
