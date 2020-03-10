c255b2794f536c432b0b8e6596f6d8a76fe787fe
swan <sc0p.sdc@gmail.com>
2020-03-10 19:46:45 +0100

    split structs

--- testing/hitrun.go
+++ testing/hitrun.go
@@ -17,6 +17,12 @@ type NPC struct {
 	py    int
 }
 
+type Wall struct {
+	*tl.Rectangle
+	px int
+	py int
+}
+
 func NewNPC(x, y, w, h int, color tl.Attr, move bool, enemy bool) *NPC {
 	return &NPC{
 		Rectangle: tl.NewRectangle(x, y, w, h, color),
@@ -29,6 +35,12 @@ func NewCharacter(x, y, w, h int, color tl.Attr, move bool) *Character {
 	return &Character{
 		Rectangle: tl.NewRectangle(x, y, w, h, color),
 		move:      move,
+	}
+}
+
+func NewWall(x, y, w, h int, color tl.Attr) *Wall {
+	return &Wall{
+		Rectangle: tl.NewRectangle(x, y, w, h, color),
 	}
 }
 
@@ -51,9 +63,15 @@ func (c *Character) Tick(ev tl.Event) {
 }
 
 func (c *Character) Collide(p tl.Physical) {
-	//if _, ok := p.(*NPC); ok && c.move {
-	if _, ok := p.(*NPC); ok {
+	if _, ok := p.(*Wall); ok {
 		c.SetPosition(c.px, c.py)
+	}
+}
+
+func (n *NPC) Collide(p tl.Physical) {
+	//if _, ok := p.(*NPC); ok && c.move {
+	if _, ok := p.(*Character); ok && n.enemy {
+		print("FIGHT")
 	}
 }
 
@@ -64,14 +82,14 @@ func main() {
 	lvl := tl.NewBaseLevel(tl.Cell{Bg: tl.ColorWhite})
 	lvl.AddEntity(NewCharacter(2, 2, 1, 1, tl.ColorYellow, true))
 	lvl.AddEntity(NewNPC(10, 9, 1, 1, tl.ColorRed, false, true))
-	lvl.AddEntity(NewNPC(8, 4, 1, 1, tl.ColorGreen, false, true))
-	lvl.AddEntity(NewNPC(8, 6, 3, 2, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(12, 7, 3, 2, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(14, 9, 3, 2, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(0, 0, 1, 20, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(0, 0, 60, 1, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(0, 20, 60, 1, tl.ColorBlack, false, false))
-	lvl.AddEntity(NewNPC(60, 0, 1, 21, tl.ColorBlack, false, false))
+	lvl.AddEntity(NewNPC(8, 4, 1, 1, tl.ColorGreen, true, false))
+	lvl.AddEntity(NewWall(8, 6, 3, 2, tl.ColorBlack))
+	lvl.AddEntity(NewWall(12, 7, 3, 2, tl.ColorBlack))
+	lvl.AddEntity(NewWall(14, 9, 3, 2, tl.ColorBlack))
+	lvl.AddEntity(NewWall(0, 0, 1, 20, tl.ColorBlack))
+	lvl.AddEntity(NewWall(0, 0, 60, 1, tl.ColorBlack))
+	lvl.AddEntity(NewWall(0, 20, 60, 1, tl.ColorBlack))
+	lvl.AddEntity(NewWall(60, 0, 1, 21, tl.ColorBlack))
 
 	game.Screen().SetLevel(lvl)
 	game.Screen().AddEntity(tl.NewFpsText(0, 22, tl.ColorRed, tl.ColorDefault, 0.5))
