3a478bd1f47e8d6df5b32b07fff541ee021d7cfc
swan <sc0p.sdc@gmail.com>
2020-08-18 16:15:04 +0200

    add post request

--- oidcTester/oidc_test.go
+++ oidcTester/oidc_test.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestConnection(t *testing.T) {
 	// Create the Request (URL, Method and Headers)
-	url := "https://idpe.post.ch/auth/sam"
+	url := "https://idpe.post.ch/auth/"
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("cache-control", "no-cache")
 
@@ -35,5 +38,41 @@ func TestConnection(t *testing.T) {
 		// --> Outputs the whole body
 		//bodyString := string(bodyBytes)
 		//t.Log(bodyString)
+	}
+}
+
+func TestBasicAuth(t *testing.T) {
+
+	searchPattern := "Your last login attempt failed. Please check your input."
+	formData := url.Values{
+		"isiwebuserid": {"testuser"},
+		"isiwebpasswd": {"testpass"},
+	}
+
+	res, err := http.PostForm("https://idpe.post.ch/auth/saml", formData)
+	if err != nil {
+		t.Error("Failed to connect")
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&result)
+
+	defer res.Body.Close()
+
+	// Check if the Response Status is OK
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		t.Log("Answer from ", res.TLS.ServerName)
+		t.Log("Status: ", res.Status)
+
+		// Check if the Login failed
+		bodyString := string(bodyBytes)
+		if strings.ContainsAny(searchPattern, bodyString) {
+			t.Error("Failed to Authenticate")
+		}
 	}
 }
